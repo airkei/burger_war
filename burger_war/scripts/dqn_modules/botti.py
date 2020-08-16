1b457369b24e10d4d5c7d88245ee79715b229aae
Nakata Keisuke <Keisuke.Nakata@panasonic.aero>
2020-08-15 17:52:19 -0700

    extend escape range

--- burger_war/scripts/dqn_modules/botti.py
+++ burger_war/scripts/dqn_modules/botti.py
@@ -297,7 +297,7 @@ class BottiNodeEnv(gazebo_env.GazeboEnv):
             done = True
 
             # emergency recovery
-            for _ in range(8):
+            for _ in range(15):
                 vel_cmd.linear.x = -self.vel_max_x
                 vel_cmd.angular.z = 0
                 self.vel_pub.publish(vel_cmd)
