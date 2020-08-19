1231ac907d29424f9d0f051db2badd578f69aa71
Nakata Keisuke <Keisuke.Nakata@panasonic.aero>
2020-08-19 14:58:06 -0700

    modify enemy point

--- burger_war/scripts/dqn_modules/run_normal.py
+++ burger_war/scripts/dqn_modules/run_normal.py
@@ -309,11 +309,11 @@ class BottiNodeEnv(gazebo_env.GazeboEnv):
         if not self.collisionMode: # production mode
             try:
                 if self.side == 'r':
-                    reward += (self.war_state_dict['scores_r'] - self.prev_score_r) * 10
+                    reward += (self.war_state_dict['scores_r'] - self.prev_score_r) * 30
                     # reward -= (self.war_state_dict['scores_b'] - self.prev_score_b) * 10
                 else:
                     # reward -= (self.war_state_dict['scores_r'] - self.prev_score_r) * 10
-                    reward += (self.war_state_dict['scores_b'] - self.prev_score_b) * 10
+                    reward += (self.war_state_dict['scores_b'] - self.prev_score_b) * 30
 
                 self.prev_score_r = self.war_state_dict['scores_r']
                 self.prev_score_b = self.war_state_dict['scores_b']
