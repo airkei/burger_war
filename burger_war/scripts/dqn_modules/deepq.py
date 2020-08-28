eddb939f48ea553dde977e1c9d2b61eb921048c1
Nakata Keisuke <Keisuke.Nakata@panasonic.aero>
2020-08-27 22:01:22 -0700

    add transfer learning

--- burger_war/scripts/dqn_modules/deepq.py
+++ burger_war/scripts/dqn_modules/deepq.py
@@ -58,6 +58,9 @@ class DeepQ:
                 model.add(Dense(layerSize, kernel_initializer='lecun_uniform', trainable=False))
                 model.add(Activation(activationType, trainable=False))
 
+            model.add(Dense(self.output_size, kernel_initializer='lecun_uniform', trainable=False))
+            model.add(Activation("linear", trainable=False))
+
             model.add(Dense(self.output_size, kernel_initializer='lecun_uniform'))
             model.add(Activation("linear"))
 
